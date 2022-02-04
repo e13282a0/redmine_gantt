256b10843f9d8c255b5007fa493b210a3258034f
Christian <christian.rust@gmx.com>
2022-02-04 17:08:40 +0100

    controller added

---
+++ app/controllers/gantt_controller.rb
@@ -0,0 +1,23 @@
+class GanttController < ApplicationController
+
+  def index
+  end
+
+  def create
+  end
+
+  def store
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
