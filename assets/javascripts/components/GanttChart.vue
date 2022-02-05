a794ca23efae345d348ca9186ddc3696edbe5688
Christian <christian.rust@gmx.com>
2022-02-05 21:02:20 +0100

    some trials

---
+++ assets/javascripts/components/GanttChart.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        ertert
+      <slot />
+
+      asdasd
+    </div>
+</template>
+
+<script>
+module.exports = {
+  props: [],
+  name:"GanttChart",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ assets/javascripts/components/GanttRow.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      {{ title }}
+    </div>
+</template>
+
+<script>
+module.exports = {
+  props: ["title"],
+  name:"GanttRow",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+
+</style>
